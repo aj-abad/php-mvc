4b3520e9255b3b20efc93f3b38acac3d6a1ae411
AJ <dev@ajabad.com>
2023-02-25 14:23:04 +0800

    Removed prefix

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -8,7 +8,7 @@ class Controller
 
   protected function view(string $viewPath): void
   {
-    $viewPath = $_SERVER['DOCUMENT_ROOT']  . '/mvc' . '/app/views/' . $viewPath . '.php';
+    $viewPath = $_SERVER['DOCUMENT_ROOT']  . '/app/views/' . $viewPath . '.php';
     if (file_exists($viewPath)) {
       require_once $viewPath;
     } else {
