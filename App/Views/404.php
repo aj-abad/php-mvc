12f85f406e98cd1ba851b963a9ad341ca06133a1
AJ <dev@ajabad.com>
2023-02-25 14:31:26 +0800

    Added basic styling

--- App/Views/404.php
+++ App/Views/404.php
@@ -6,10 +6,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Not found</title>
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
 </head>
 
-<body>
-  Not found
+<body style="height: 100vh" class="d-flex justify-content-center align-items-center">
+  <div class="text-center">
+    <h1 style="font-size: 10vw;line-height: 10vw;">
+      Not found
+    </h1>
+    <p style="font-size: 2rem">
+      No route found for <code><?php echo htmlspecialchars($requestUri) ?></code>
+    </p>
+  </div>
 </body>
 
 </html>
