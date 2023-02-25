10f781509e3402313b89ce44b81d79258d911640
AJ <dev@ajabad.com>
2023-02-25 16:19:54 +0800

    Added data to view method

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -4,12 +4,20 @@ namespace App\Controllers;
 
 class Controller
 {
-  protected function view(string $viewPath): void
+  protected function view(string $viewPath, ?array $data = null): void
   {
     $viewPath = $_SERVER['DOCUMENT_ROOT']  . '/app/views/' . $viewPath . '.php';
+
     if (!file_exists($viewPath)) {
       throw new \Exception('View not found: ' . $viewPath);
     }
+
+    if ($data) {
+      foreach ($data as $key => $value) {
+        $$key = htmlspecialchars($value);
+      }
+    }
+
     require_once $viewPath;
   }
 
