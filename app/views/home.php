f97d45ca4cae799f62fa3bcbe68e76c4ac2748bf
AJ <dev@ajabad.com>
2023-02-28 00:13:30 +0800

    Added some styles

--- app/views/home.php
+++ app/views/home.php
@@ -1,8 +1,15 @@
+<?php
+
+use App\Modules\Route; ?>
+
 <div class="container">
   <h1>
     Home
   </h1>
-  <p>
+  <p class="mb-4">
     This is the home page
+  </p>
+  <p>
+    <a href="<?= Route::getNamed('about')->route ?>">About page</a>
   </p>
 </div>
