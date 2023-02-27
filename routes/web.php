da45bae22a720adb81abbe8a389c70b7db4938ae
AJ <dev@ajabad.com>
2023-02-28 00:13:23 +0800

    Changed test method names

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Modules\Route;
 
 Route::register(RequestMethod::GET, "/", HomeController::class, "index")->named("home");
 Route::register(RequestMethod::GET, "/about", HomeController::class, "about")->named("about");
-Route::register(RequestMethod::GET, "/test/:id/add", HomeController::class, "test")->named("about");
-Route::register(RequestMethod::GET, "/:id/test", HomeController::class, "test")->named("about");
+Route::register(RequestMethod::GET, "/test/:id/add", HomeController::class, "test")->named("test1");
+Route::register(RequestMethod::GET, "/:id/test", HomeController::class, "test")->named("test2");
