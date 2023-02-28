f745891c11800a1f979275ac605a54895caf9719
AJ <dev@ajabad.com>
2023-03-01 02:38:10 +0800

    Added namespace and fillable attributes

--- app/models/Model.php
+++ app/models/Model.php
@@ -1,6 +1,22 @@
 <?php
 
+namespace App\Models;
+
 class Model
 {
   protected $fillable = [];
+
+  public function __construct(array $attributes = [])
+  {
+    $this->fill($attributes);
+  }
+
+  public function fill(array $attributes)
+  {
+    foreach ($attributes as $key => $value) {
+      if (in_array($key, $this->fillable)) {
+        $this->{$key} = $value;
+      }
+    }
+  }
 }
