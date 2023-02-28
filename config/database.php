9ce3594977a97a1fcf3f48b23de6cd8bdd00bf4a
AJ <dev@ajabad.com>
2023-03-01 04:03:52 +0800

    Changed default table name

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
   "DB_HOST" => getenv("DB_HOST") ?? "localhost",
-  "DB_NAME" => getenv("DB_NAME") ?? "test",
+  "DB_NAME" => getenv("DB_NAME") ?? "lifescan",
   "DB_USER" => getenv("DB_USER") ?? "root",
   "DB_PASS" => getenv("DB_PASS") ?? "",
 ];
