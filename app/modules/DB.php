572cc4fb1d551d049aafeeb49d101e33c79fece4
AJ <dev@ajabad.com>
2023-03-01 01:50:10 +0800

    Added database facade class

--- app/modules/DB.php
+++ app/modules/DB.php
@@ -1,9 +1,25 @@
 <?php
 
+namespace App\Modules;
+
 class DB
 {
-  public function connect()
+  public static function connect()
   {
-    // connect to database
+    $config = require_once $_SERVER["DOCUMENT_ROOT"] . "/config/database.php";
+    $dsn = "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']}";
+    $pdo = new \PDO($dsn, $config['DB_USER'], $config['DB_PASS']);
+    $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    return $pdo;
+  }
+
+  public static function query($sql, $params = [])
+  {
+    $stmt = self::connect()->prepare($sql);
+    $queryResult = $stmt->execute($params);
+    if ($queryResult) {
+      return $stmt->fetchAll();
+    }
+    return null;
   }
 }
