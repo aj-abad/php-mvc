3a254be3b2b8c7482179d300766c1e2ae99f5d2e
AJ <dev@ajabad.com>
2023-03-02 13:39:46 +0800

    Added auth test on about page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
+
 use App\Controllers\HomeController;
+use App\Middleware;
 use App\Modules\RequestMethod;
 use App\Modules\Route;
 
 Route::register(RequestMethod::GET, "/", HomeController::class, "index")->named("home");
-Route::register(RequestMethod::GET, "/about", HomeController::class, "about")->named("about");
+Route::register(RequestMethod::GET, "/about", HomeController::class, "about")->named("about")->middleware([Middleware\AuthMiddleware::class]);
 Route::register(RequestMethod::POST, "/", HomeController::class, "create")->named("home.post");
 Route::register(RequestMethod::POST, "/sign-in", HomeController::class, "signIn")->named("signin");
