cc806cc91feae5a6945e975104028828a1f70f55
AJ <dev@ajabad.com>
2023-03-02 13:01:31 +0800

    Added auth facade methods

--- app/modules/Auth.php
+++ app/modules/Auth.php
@@ -4,4 +4,18 @@ namespace App\Modules;
 
 class Auth
 {
+  public static function isLoggedIn()
+  {
+    return isset($_SESSION["user"]);
+  }
+
+  public static function login(array $user)
+  {
+    $_SESSION["user"] = $user;
+  }
+
+  public static function user()
+  {
+    return @$_SESSION["user"] ?? null;
+  }
 }
